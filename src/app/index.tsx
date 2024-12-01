2e70b51810624bb4f5d9d6359c4cfefa35acffcd
hissashi <114583726+Hissashi43@users.noreply.github.com>
2024-12-01 11:09:24 +0900

    styling index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,17 +6,18 @@ const Index = (): JSX.Element => {
 
       <View style={styles.header}>
         <View style={styles.headerInner}>
+          <Text style={styles.headerLeft}>戻る</Text>
           <Text style={styles.headerTitle}>Diary App</Text>
           <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
-      <View>
-        <Text>10月</Text>
+      <View style={styles.monthTitle}>
+        <Text style={styles.monthText}>10月</Text>
       </View>
 
-      <View>
-        <Text>10月24日（金）</Text>
+      <View style={styles.date}>
+        <Text style={styles.dateText}>10月24日（金）</Text>
       </View>
 
       <View style={styles.imageContainer}>
@@ -29,8 +30,12 @@ const Index = (): JSX.Element => {
           source={require('../../assets/example-image.png')}/>
       </View>
 
-      <View>
-        <Text>山梨県の富士五胡の一つ、河口湖から富士山を見た。ちょうど紅葉真っ只中でなかなかのいい景色が取れた。なおこの場所はかなり有名らしく、写真を撮ろうと多くの人がごった返しており人が映らないような場所とタイミングを見極めるのにかなり苦労した</Text>
+      <View style={styles.diaryContent}>
+        <Text style={styles.diaryContentText}>山梨県の富士五胡の一つ、河口湖から富士山を見た。ちょうど紅葉真っ只中でなかなかのいい景色が取れた。なおこの場所はかなり有名らしく、写真を撮ろうと多くの人がごった返しており人が映らないような場所とタイミングを見極めるのにかなり苦労した</Text>
+      </View>
+
+      <View style={styles.circleButton}>
+          <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
 
@@ -58,6 +63,13 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     opacity: 0.7
   },
+  headerLeft: {
+    position: 'absolute',
+    left: 16,
+    bottom: 16,
+    color: '#ffffff',
+    opacity: 0.7
+  },
   headerTitle: {
     marginBottom: 8,
     color: '#ffffff',
@@ -67,6 +79,61 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     alignItems: 'center'
+  },
+  monthTitle: {
+    backgroundColor: '#BF5D5D',
+    height: 51,
+    width: 368,
+    marginBottom: 8,
+    marginTop: 8,
+    marginLeft: 17,
+    marginRight: 17,
+    alignItems: 'center'
+  },
+  monthText: {
+    fontSize: 32,
+    color: '#ffffff',
+    fontWeight: 'bold',
+    justifyContent: 'flex-end',
+    marginBottom: 8
+  },
+  date: {
+    marginLeft: 18
+  },
+  dateText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8
+  },
+  diaryContent: {
+    alignItems: 'center',
+    marginRight: 17,
+    marginLeft: 17
+  },
+  diaryContentText: {
+    fontSize: 18,
+    lineHeight: 28
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#55E42E',
+    position: 'absolute',
+    right: 24,
+    bottom: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: {width: 0, height: 8},
+    elevation: 8
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 48
   }
 })
 
