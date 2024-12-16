d45efc593c730fe3a590c646d0647c9698ee8618
hissashi <114583726+Hissashi43@users.noreply.github.com>
2024-12-17 08:09:40 +0900

    apply monthcolor to diary

--- src/app/diary/diary.tsx
+++ src/app/diary/diary.tsx
@@ -20,6 +20,21 @@ const Diary = (): JSX.Element => {
   const dateDirectory = date?.replace('-', '').replace('-', '')
   const [diaryData, setDiaryData] = useState<{ bodyText: string; updatedAt: string } | null>(null)
   const [loading, setLoading] = useState(true)
+  const monthColors: { [key: string]: string } = {
+    '01': '#F65E5E', // 1月: 明るい赤
+    '02': '#5CA1DD', // 2月: 明るい青
+    '03': '#F893E2', // 3月: 明るいピンク
+    '04': '#64DA51', // 4月: 明るい緑
+    '05': '#67C09F', // 5月: 明るいシアン
+    '06': '#B47BDA', // 6月: 明るい紫
+    '07': '#49D1E1', // 7月: 明るい空色
+    '08': '#E58027', // 8月: 明るいオレンジ
+    '09': '#3DC02E', // 9月: 明るいライム
+    '10': '#E1C84A', // 10月: 明るい黄色
+    '11': '#BF5D5D', // 11月: 明るいローズ
+    '12': '#4F69BF' // 12月: 明るい青紫
+  }
+  const currentBackgroundColor = monthColors[month] || '#000000'
 
   useEffect(() => {
     const fetchDiaryData = async () => {
@@ -72,7 +87,7 @@ const Diary = (): JSX.Element => {
     <View style={styles.container}>
 
 
-      <View style={styles.monthTitle}>
+      <View style={[styles.monthTitle, { backgroundColor: currentBackgroundColor }]}>
         <Text style={styles.monthText}>
           {date ? `${month}月` : 'unknown date'}
         </Text>
